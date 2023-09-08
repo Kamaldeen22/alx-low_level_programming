ef8767ee0e9487a8844011c88a8c327272966a69
Kamaldeen22 <kamaldeenawwal37@gmail.com>
2023-09-08 14:53:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Empty point
+ * Description: 'Print alphabets lowercase'
+ * return: Always 0
+ */
+int main(void)
+{
+	int m = 97;
+
+	while (m <= 122)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
