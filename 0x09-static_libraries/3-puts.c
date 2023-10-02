56ad8874df75326a166325a6327addb31abd47de
Kamaldeen22 <kamaldeenawwal37@gmail.com>
2023-10-02 11:43:40 -0700

    statics

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -6,11 +6,11 @@
  */
 void _puts(char *str)
 {
-int c;
+	int c;
 
-for (c = 0; str[c] != 0; c++)
-{
-_putchar(str[c]);
-}
-_putchar('\n');
+	for (c = 0; str[c] != 0; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
 }
